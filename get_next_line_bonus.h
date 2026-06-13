953c7d22e197a3b1aefb1a090170a14d3464ac79
makif38 <akifkarakaya.kayseri@gmail.com>
2026-06-13 21:20:29 +0200

    gnl bonus.h

---
+++ get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akkaraka <akkaraka@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/13 20:51:48 by akkaraka          #+#    #+#             */
+/*   Updated: 2026/06/13 21:03:16 by akkaraka         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# define MAX_FD 1024
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(const char *s1, const char *s2);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlen(const char *s);
+
+# endif
